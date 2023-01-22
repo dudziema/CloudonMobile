30ceaf6a92d978f8441d53fd5bf0df4c84ac7e83
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-01-22 22:57:30 +0100

    Refactor

--- tests/unit/BaseUpload.spec.ts
+++ tests/unit/BaseUpload.spec.ts
@@ -2,18 +2,17 @@ import { mount } from '@vue/test-utils'
 import BaseUpload from '@/components/ui/BaseUpload.vue'
 import { createContext } from '@/composables/context'
 
+const { context } = createContext()
+
 describe('BaseUpload', () => {
   it('renders correctly', () => {
-    const { context } = createContext()
     const wrapper = mount(BaseUpload, {
       global: {
         provide: {
           context: context
         }
       }
-    });
-
-    // Assertions
+    })
     expect(wrapper.element).toMatchSnapshot()
   })
 })
