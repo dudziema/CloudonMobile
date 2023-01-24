4bac16c95869c43200f9efd3c879396008562644
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-01-24 11:15:03 +0100

    Finish unit tests for BaseUpload component

--- tests/unit/BaseUpload.spec.ts
+++ tests/unit/BaseUpload.spec.ts
@@ -1,18 +1,65 @@
-import { mount } from '@vue/test-utils'
+import { mount, VueWrapper } from '@vue/test-utils'
 import BaseUpload from '@/components/ui/BaseUpload.vue'
 import { createContext } from '@/composables/context'
 
 const { context } = createContext()
+const MAX_SIZE_FILE_IN_BYTES = 104857600
+const ONE_MEGABYTE = 1048576
 
 describe('BaseUpload', () => {
-  it('renders correctly', () => {
-    const wrapper = mount(BaseUpload, {
+  let wrapper: VueWrapper<any>
+  
+  beforeEach(() => {
+    wrapper = mount(BaseUpload, {
       global: {
         provide: {
           context: context
         }
       }
     })
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('renders correctly', () => {
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('isFileValid returns true when file size is less than 100MB', () => {
+    const file = { size: MAX_SIZE_FILE_IN_BYTES - ONE_MEGABYTE }
+    const result = wrapper.vm.isFileValid(file)
+    expect(result).toBe(true)
+  })
+
+  it('isFileValid returns true when file size is more than 100MB', () => {
+    const file = { size: MAX_SIZE_FILE_IN_BYTES + ONE_MEGABYTE }
+    const result = wrapper.vm.isFileValid(file)
+    expect(result).toBe(false)
+  })
+
+  it('handleFileChange calls webSocketService.sendFile when file is valid', () => {
+    const file = { size: MAX_SIZE_FILE_IN_BYTES - ONE_MEGABYTE }
+    const event = { target: { files: [file] } }
+    const spy = jest.spyOn(context.webSocketService, 'sendFile')
+    wrapper.vm.handleFileChange(event)
+    expect(spy).toHaveBeenCalledWith(file)
+  })
+
+  it('handleFileChange calls webSocketService.sendFile when file is invalid', () => {
+    const file = { size: MAX_SIZE_FILE_IN_BYTES + ONE_MEGABYTE }
+    const event = { target: { files: [file] } }
+    const spy = jest.spyOn(context.webSocketService, 'sendFile')
+    wrapper.vm.handleFileChange(event)
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  test('handleFileChange should not call webSocketService.sendFile when there is no file selected', () => {
+    const mockSendFile = jest.fn()
+    context.webSocketService.sendFile = mockSendFile
+    const event = { target: { files: [] } }
+    wrapper.vm.handleFileChange(event)
+    expect(mockSendFile).not.toHaveBeenCalled()
   })
 })
