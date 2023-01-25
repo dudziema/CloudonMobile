6742aaa20739ab4e7cc3e46def161ef9c7ab7001
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-01-25 18:18:03 +0100

    Add snapshots to BaseButton

--- tests/unit/BaseButton.spec.ts
+++ tests/unit/BaseButton.spec.ts
@@ -6,12 +6,14 @@ describe('BaseButton', () => {
     const theme = 'active'
     const wrapper = shallowMount(BaseButton, { props: { theme }})
     expect(wrapper.attributes('class')).toBe('base-button base-button--active')
+    expect(wrapper.element).toMatchSnapshot()
   })
 
   it('renders with inactive theme when passed as prop', () => {
     const theme = 'inactive'
     const wrapper = shallowMount(BaseButton, { props: { theme }})
     expect(wrapper.attributes('class')).toBe('base-button base-button--inactive')
+    expect(wrapper.element).toMatchSnapshot()
   })
 
   it('renders with new-file theme when passed as prop', () => {
