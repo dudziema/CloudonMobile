fc41af71722d09acdd0e146288e56ef4c5c2bc73
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-01-25 17:11:03 +0100

    Add FileItem component unit test

---
+++ tests/unit/FileItem.spec.ts
@@ -0,0 +1,43 @@
+import { mount, VueWrapper } from '@vue/test-utils'
+import { createContext } from '@/composables/context'
+import FileItem from '@/components/ui/FileItem.vue'
+import File from '@/types/File'
+
+const { context } = createContext()
+
+describe('FileItem', () => {
+  let wrapper: VueWrapper<any>
+  const file: File = { name: 'example.jpg', filename: 'example.jpg', size: 1000000, dateEpoch: 1610733200000 }
+
+  beforeEach(() => {
+    wrapper = mount(FileItem, {
+      props: { file },
+      global: {
+        provide: {
+          context: context
+        }
+      }})
+  })
+  it('should render the correct file name', () => {
+    const name = wrapper.find('[data-testid=file-name]')
+    expect(name.exists()).toBeTruthy()
+    expect(name.text()).toContain(file.name)
+  })
+
+  it('should render the correct file size', () => {
+    const size = wrapper.find('[data-testid=file-size]')
+    expect(size.exists()).toBeTruthy()
+    expect(size.text()).toContain('977 KB')
+  })
+
+  it('should render the correct file date', () => {
+    const date = wrapper.find('[data-testid=file-dateEpoch]')
+    expect(date.exists()).toBeTruthy()
+    expect(date.text()).toContain('15.01.2021')
+  })
+
+  it('should render the correct file icon', () => {
+    const icon = wrapper.find('[data-testid=file-icon]')
+    expect(icon.exists()).toBeTruthy()
+  })
+})
