e29564e7685fc8078c8447c37ad51247356964b4
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-02-09 20:41:16 +0100

    Update WS service with new types

--- src/classes/services/WebSocketService.ts
+++ src/classes/services/WebSocketService.ts
@@ -1,4 +1,3 @@
-import Message from '@/types/Message'
 import MessageCommands from '@/types/MessageCommands'
 import MessageTypes from '@/types/MessageTypes'
 import File from '@/types/File'
@@ -6,25 +5,25 @@ import ContentType from '@/types/ContentType'
 import base64ToArrayBuffer from '@/utils/helpers/base64ToArrayBuffer'
 import { Buffer } from 'buffer'
 import MessageSent from '@/types/MessageSent'
+import MessageReceived from '@/types/MessageReceived'
 
 export class WebSocketService {
-  listFiles = [] as File[]
-  wsOnMessageListeners: ((obj: Message) => void)[] = []
+  listFiles = [] as File[] | File | undefined
+  wsOnMessageListeners: ((obj: MessageReceived) => void)[] = []
   private ws: WebSocket | undefined
-  private passCode: number | undefined
-  private wsOnMessageListenersListFiles: ((listfiles: File[]) => void) | null = null
+  private passcode: number | undefined
+  private wsOnMessageListenersListFiles: ((listfiles: File[]| File | undefined) => void) | null = null
 
   onOpen = () => {
     console.log('WS opened')
     this.sendMsgToWs({
       type: MessageTypes.LOGING_WITH_CODE,
-      code: this.passCode,
+      code: this.passcode,
     })
   }
 
   onMessage = (event: MessageEvent<string>) => {
-    let receivedMessage: string = event.data
-    this.parseMessage(receivedMessage)
+    this.parseMessage(JSON.parse(event.data))
   }
 
   onError = (error: Event) => {
@@ -36,9 +35,9 @@ export class WebSocketService {
     console.log('socket closed' + JSON.stringify(event))
   }
   
-  login(passCode: number) {
+  login(passcode: number) {
     console.log('Starting connection to WebSocket Server')
-    this.passCode = passCode
+    this.passcode = passcode
     this.ws = new WebSocket('wss://cloudon.cc:9292/')
     this.ws.onopen = this.onOpen
     this.ws.onmessage = this.onMessage
@@ -47,21 +46,19 @@ export class WebSocketService {
   }
 
   downloadFile(fileName: string) {
-    let msg: MessageSent = {
+    this.sendMsgToWs({
       type: MessageTypes.FORWARD,
       command: MessageCommands.DOWNLOAD,
       path: fileName,
-    }
-    this.sendMsgToWs(msg)
+    })
   }
 
   deleteFile(fileName: string) {
-    let msg: MessageSent = {
+    this.sendMsgToWs({
       type: MessageTypes.FORWARD,
       command: MessageCommands.REMOVE,
       path: fileName,
-    }
-    this.sendMsgToWs(msg)
+    })
     this.wsListFiles()
   }
 
@@ -72,8 +69,8 @@ export class WebSocketService {
         
     reader.onloadend = () => {
       if (reader.readyState === FileReader.DONE) {
-        let data = reader.result as string
-        let base64String = Buffer.from(data).toString('base64')
+        const data = reader.result as string
+        const base64String = Buffer.from(data).toString('base64')
         this.wsUploadFile(file.name, file.size, base64String)
       }
     }
@@ -83,7 +80,7 @@ export class WebSocketService {
     this.ws?.close()
   }
 
-  addWsOnMessageListener( listenerFunction: any ) {
+  addWsOnMessageListener( listenerFunction: ((obj: MessageReceived) => void) ) {
     this.wsOnMessageListeners.push(listenerFunction)
   }
 
@@ -91,7 +88,7 @@ export class WebSocketService {
     this.ws?.send(JSON.stringify(msg))
   }
 
-  private parseListFiles(obj: { payload: File[] }) {
+  private parseListFiles(obj: MessageReceived) {
     this.listFiles = obj.payload
 
     if (this.wsOnMessageListenersListFiles) {
@@ -99,55 +96,68 @@ export class WebSocketService {
     }
   }
 
-  private saveByteArray(fileName: string, decodedBytes: Uint8Array) {
-    let mimeType = ContentType.OCTET_STREAM
+  private saveByteArray(fileName: string | undefined, decodedBytes: Uint8Array) {
+    const mimeType = ContentType.OCTET_STREAM
     const blob = new Blob([decodedBytes], { type: mimeType })
-    const link = document.createElement('a')
+    let link = document.createElement('a')
     link.href = window.URL.createObjectURL(blob)
-    link.download = fileName
-    link.click()
-  }
 
-  private onDownloadedFileFromPhone(message: Message) {
-    if(message.payload) {
-      let decodedBytes = base64ToArrayBuffer(message.payload.bytes)
-      this.saveByteArray(message.payload.filename, decodedBytes)
+    if(fileName) {
+      link.download = fileName
+      link.click()
     }
   }
 
-  private parseMessage(receivedMessage: string) {
-    let obj = JSON.parse(receivedMessage)
+  isFileArray(files: File | File[]): files is File[] {
+    return Array.isArray(files)
+  }
 
-    if (obj.type === MessageTypes.LOGING_WITH_CODE) {
+  private onDownloadedFileFromPhone(message: MessageReceived) {
+    if(message.payload) {
+      if(this.isFileArray(message.payload)) {
+        message.payload.forEach(file => {
+          const decodedBytes = base64ToArrayBuffer(file.bytes)
+          this.saveByteArray(file.filename, decodedBytes)
+        })
+
+      } else {
+        const decodedBytes = base64ToArrayBuffer(message.payload.bytes)
+        this.saveByteArray(message.payload.filename, decodedBytes)
+      }
+    }
+  }
+
+  private parseMessage(receivedMessage: MessageReceived) {
+    if (receivedMessage.type === MessageTypes.LOGING_WITH_CODE) {
       if (this.wsOnMessageListeners) {
         this.wsOnMessageListeners.forEach(listener => {
-          listener(obj)
+          listener(receivedMessage)
         })
       }
     }
-    let message: MessageCommands = obj.command
+    const messageCommand: MessageCommands | undefined = receivedMessage.command
 
-    switch (message) {
+    switch (messageCommand) {
     case MessageCommands.DOWNLOAD:
-      this.onDownloadedFileFromPhone(obj)
+      this.onDownloadedFileFromPhone(receivedMessage)
       break
     case MessageCommands.UPLOAD:
       this.wsListFiles()
       break
     case MessageCommands.LIST_FILES:
-      this.parseListFiles(obj)
+      this.parseListFiles(receivedMessage)
       break
     }
   }
 
-  private wsUploadFile(filename: string, size: number, base64String: string) {
-    let msg = {
+  private wsUploadFile(filename: string | undefined, size: number, base64String: string) {
+    this.ws?.send(JSON.stringify({
       type: MessageTypes.FORWARD,
       command: MessageCommands.UPLOAD,
       payload: { filepath: filename, path: '', size: size, bytes: base64String },
-    }
-    this.ws?.send(JSON.stringify(msg))
+    }))
   }
+
   private wsListFiles(func?: () => void) {
     if (func) this.wsOnMessageListenersListFiles = func
     this.ws?.send(
