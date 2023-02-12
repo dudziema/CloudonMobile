08231323fe24a2effa67dc0bc3890849d5bc4ad9
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-02-12 18:30:19 +0100

    Add cancel search button

--- src/components/ui/SearchBar.vue
+++ src/components/ui/SearchBar.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, Ref } from 'vue'
 import IconSearch from '@/assets/images/search/IconSearch.svg'
+import ImageX from '@/assets/images/modal/x.svg'
 
-const searchInput: string = ref()
+const searchInput: Ref<string> = ref()
 const emit = defineEmits<{
   (e: 'search', value: string): void
 }>()
 
 function search(searchFile: string) {
   emit('search', searchFile)
+}
+
+function clearSearchInput() {
+  searchInput.value = ''
+  search(searchInput.value)
 }
 </script>
 
@@ -21,18 +27,23 @@ function search(searchFile: string) {
       placeholder="Search for anything"
       @keyup.prevent="search(searchInput)"
     />
-    <IconSearch class="search-bar__img"/>
+    <IconSearch class="search-bar__img" />
+    <ImageX
+      class="search-bar__cancel"
+      @click="clearSearchInput"
+    />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .search-bar {
   position: relative;
+  width: 80%;
 
   &__input {
     border: 1px solid transparent;
     border-radius: 8px;
-    width: 80%;
+    width: 100%;
     padding: 9px 4px 9px 40px;
     background: transparent;
 
@@ -55,5 +66,17 @@ function search(searchFile: string) {
     top: 10px;
     opacity: 0.4;
   }
+  &__cancel {
+    position: absolute;
+    right: 12px;
+    top: 10px;
+  }
+}
+
+input[type="search"]::-webkit-search-decoration,
+input[type="search"]::-webkit-search-cancel-button,
+input[type="search"]::-webkit-search-results-button,
+input[type="search"]::-webkit-search-results-decoration {
+  -webkit-appearance:none;
 }
 </style>
